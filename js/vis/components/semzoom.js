5c221a1adf25869e5bc616f4256a09cf2a431f80
Michael Brooks <mjbrooks@uw.edu>
2013-04-20 17:06:37 -0700

    refactor semzoom

--- js/vis/components/semzoom.js
+++ js/vis/components/semzoom.js
@@ -4,10 +4,10 @@ define(['underscore', 'lib/d3'],
         /**
          * Class for managing semantic zooming.
          *
-         * This listens to changes in from the d3 zoom behavior.
-         *
-         * When the visible time range becomes too large or small,
-         * it triggers a request for data at a new granularity.
+         * While monitoring a scale whose domain and range
+         * are being used to display data, it can be used
+         * to determine if the current data interval and bin width
+         * needs to be updated.
          */
         var SemanticZoom = function() {
             this._dataInterval = [0,1];
@@ -39,86 +39,85 @@ define(['underscore', 'lib/d3'],
                 return this;
             },
 
+            _is_center_off: function(visibleDomain, dataInterval) {
+                var domainCenter = (visibleDomain[0] + visibleDomain[1]) * 0.5;
+                var dataCenter = (dataInterval[0] + dataInterval[1]) * 0.5;
+                var domainWidth = visibleDomain[1] - visibleDomain[0];
+
+                var centerOffset = Math.abs((domainCenter - dataCenter) / domainWidth);
+
+                return centerOffset > 0.5;
+            },
+
+            _is_close_to_edge: function(visibleDomain, dataInterval) {
+                var domainWidth = visibleDomain[1] - visibleDomain[0];
+
+                var dataBufferMinWidth = Math.min(dataInterval[1] - visibleDomain[1],
+                    visibleDomain[0] - dataInterval[0]) / domainWidth;
+
+                return dataBufferMinWidth < 0.25;
+            },
+
+            _is_bin_count_wrong: function(visibleDomain, binWidth) {
+                var domainWidth = visibleDomain[1] - visibleDomain[0];
+
+                var visibleBins = domainWidth / binWidth;
+                var distanceFromIdeal = Math.abs(visibleBins - this._idealBinCount) / this._idealBinCount;
+                //If we've deviated from the ideal by over 50%, adjust the granularity
+                return distanceFromIdeal > 0.5;
+            },
+
+            _recalculate: function(visibleDomain, dataInterval, binWidth) {
+                var domainWidth = visibleDomain[1] - visibleDomain[0];
+
+                //Use the tick calculation code at the bottom to
+                //generate the new interval and bin width.
+                var desiredDataInterval = [visibleDomain[0] - domainWidth, visibleDomain[1] + domainWidth];
+                var desiredDataPoints = this._idealBinCount * 3
+                var settings = sz_ticks(desiredDataInterval, desiredDataPoints);
+
+                var newDataInterval = [+settings[0], +settings[1]];
+                var newBinWidth = settings[2];
+
+                if (newDataInterval[0] != dataInterval[0] ||
+                    newDataInterval[1] != dataInterval[1] ||
+                    newBinWidth != binWidth) {
+                    return {
+                        interval: newDataInterval,
+                        binWidth: newBinWidth
+                    }
+                }
+                
+                return null;
+            },
+
             /**
              * Check to see if an update is recommended.
              *
-             * Returns true if the bin width and data interval being shown
-             * should be changed.
+             * Provide the interval and binWidth for the currently loaded data.
+             *
+             * If the bin width and data interval being shown should be changed,
+             * it returns an object containing the new interval and binWidth.
              */
-            update: function() {
+            recommend: function(interval, binWidth) {
                 var rawDomain = this._scale.domain();
                 var visibleDomain = [+rawDomain[0], +rawDomain[1]];
 
                 var recalculate = false;
 
-                //Check the data interval to see if we are close to the edge
-                var domainCenter = (visibleDomain[0] + visibleDomain[1]) * 0.5;
-                var domainWidth = visibleDomain[1] - visibleDomain[0];
-                var dataCenter = (this._dataInterval[0] + this._dataInterval[1]) * 0.5;
+                //Check to see if we are close to the edge
+                recalculate || (recalculate = this._is_close_to_edge(visibleDomain, interval));
 
-                var centerOffset = Math.abs((domainCenter - dataCenter) / domainWidth);
-                var dataBufferMinWidth = Math.min(this._dataInterval[1] - visibleDomain[1],
-                    visibleDomain[0] - this._dataInterval[0]) / domainWidth;
-                if (centerOffset > 0.5 || dataBufferMinWidth < 0.25) {
-                    //We've shifted by over 50% of the visible domain, so adjust the data interval
-                    recalculate = true;
-                }
+                //Check to see if the center is way off
+                recalculate || (recalculate = this._is_center_off(visibleDomain, interval));
 
                 //Check the number of visible bins to see if we need to load more/less data
-                var visibleBins = domainWidth / this._binWidth;
-                var distanceFromIdeal = Math.abs(visibleBins - this._idealBinCount) / this._idealBinCount;
-                if (distanceFromIdeal > 0.5) {
-                    //We've deviated from the ideal by over 50%, so adjust the granularity
-                    recalculate = true;
-                }
+                recalculate || (recalculate = this._is_bin_count_wrong(visibleDomain, binWidth));
 
                 if (recalculate) {
-                    //Use the tick calculation code at the bottom to
-                    //generate the new interval and bin width.
-                    var desiredDataInterval = [visibleDomain[0] - domainWidth, visibleDomain[1] + domainWidth];
-                    var desiredDataPoints = this._idealBinCount * 3
-                    var settings = sz_ticks(desiredDataInterval, desiredDataPoints);
-
-                    var newDataInterval = [+settings[0], +settings[1]];
-                    var newBinWidth = settings[2];
-
-                    if (newDataInterval[0] != this._dataInterval[0] ||
-                        newDataInterval[1] != this._dataInterval[1] ||
-                        newBinWidth != this._binWidth) {
-                        this._dataInterval = newDataInterval;
-                        this._binWidth = newBinWidth;
-                        return true;
-                    }
+                    return this._recalculate(visibleDomain, interval, binWidth);
                 }
                 return false;
-            },
-
-            /**
-             * Get or set the data range.
-             *
-             * Set this before update, and after update it will contain the
-             * recommended value.
-             */
-            interval: function(interval) {
-                if (!arguments.length) {
-                    return this._dataInterval;
-                }
-                this._dataInterval = interval;
-                return this;
-            },
-
-            /**
-             * Get or set the data bin width.
-             *
-             * Set this before update, and after update it will contain
-             * the recommended value.
-             */
-            binWidth: function(width) {
-                if (!arguments.length) {
-                    return this._binWidth;
-                }
-                this._binWidth = width;
-                return this;
             }
 
         });
--- js/vis/components/zoom_histogram.js
+++ js/vis/components/zoom_histogram.js
@@ -117,20 +117,18 @@ define(['underscore',
             update: function() {
                 var self = this;
 
-                //Tell the semantic zoom controller about our current status
-                this._semantic
-                .interval(this.extent())
-                .binWidth(this.zoomLevel())
-
+                console.log(this.extent().toString());
                 //Check if the semantic zoom controller recommends new data.
-                var changed = this._semantic.update();
-
+                var change = this._semantic.recommend(this.extent(), this.zoomLevel());
+                console.log(change);
                 var alreadyUpdated = false;
 
-                if (changed) {
+                if (change) {
                     //Ask the semantic zoom controller what the new data should cover
-                    var binWidth = this._semantic.binWidth();
-                    var interval = this._semantic.interval()
+                    var binWidth = change.binWidth;
+                    var interval = change.interval;
+
+                    console.log(this.extent()[0] - interval[0]);
 
                     //Save the current zoom level and extent
                     //This prevents multiple redundant loads while waiting.
