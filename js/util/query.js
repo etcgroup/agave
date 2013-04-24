47bae14fc8a3216c61929c96bec7b6a88b1ea00b
Michael Brooks <mjbrooks@uw.edu>
2013-04-24 00:07:09 -0700

    add query and tests, update jasmine-jquery

---
+++ js/util/query.js
@@ -0,0 +1,170 @@
+define(['jquery', 'underscore', 'util/events'], function ($, _, events) {
+
+    var DEFAULT_DATA = {
+        view: 'area',
+        search: '',
+        author: '',
+        rt: false,
+        min_rt: 0,
+        sentiment: ''
+    };
+
+    var VALID_VIEW_MODES = ['area', 'stacked', 'expand', 'hidden'];
+
+    var VALID_SENTIMENTS = ['', 'negative', 'neutral', 'positive'];
+
+    /**
+     * A class for managing query settings, updating a set
+     * of query controls, and updating the URL.
+     *
+     * @param ui
+     * @param data
+     * @constructor
+     */
+    var Query = function (ui, data) {
+        data = data || {};
+        this.data = _.defaults(data, DEFAULT_DATA);
+
+        this._initUI(ui);
+        this._fillForm();
+
+        this._initEvents();
+    }
+
+    //Add 'on' and 'trigger'
+    events(Query);
+
+    /**
+     * Find several important UI elements.
+     *
+     * @param form
+     * @private
+     */
+    Query.prototype._initUI = function (form) {
+        this.ui = {};
+
+        this.ui.form = form;
+
+        this.ui.view_buttons = form.find('.view-buttons');
+        this.ui.search_input = form.find('.query-search');
+        this.ui.author_input = form.find('.query-author');
+        this.ui.rt_checkbox = form.find('.query-rt');
+        this.ui.min_rt_input = form.find('.query-minrt');
+        this.ui.sentiment_select = form.find('.query-sentiment');
+        this.ui.update_button = form.find('.query-update');
+    }
+
+    /**
+     * Set up handlers for UI events.
+     *
+     * @private
+     */
+    Query.prototype._initEvents = function () {
+        this.ui.update_button.on('click', $.proxy(this._updateClicked, this));
+        this.ui.view_buttons.on('click', $.proxy(this._viewButtonClicked, this));
+        this.ui.form.on('submit', $.proxy(this._formSubmitted, this));
+    }
+
+    /**
+     * Called when the update button is clicked for this query.
+     *
+     * @private
+     */
+    Query.prototype._updateClicked = function () {
+        if (this.collectData()) {
+            this.trigger('update', this);
+        }
+    }
+
+    /**
+     * Called when a view control button is clicked.
+     *
+     * @private
+     */
+    Query.prototype._viewButtonClicked = function () {
+        this.trigger('view-change', this);
+    }
+
+    /**
+     * Called when the form is submitted (probably user pressed enter).
+     *
+     * @private
+     */
+    Query.prototype._formSubmitted = function (e) {
+        e.preventDefault();
+        this._updateClicked();
+        return false;
+    }
+
+    /**
+     * Gather user input from the query controls.
+     */
+    Query.prototype.collectData = function () {
+
+        //Get the mode of the active mode button (validate)
+        var mode = this.ui.view_buttons.find('.active').data('mode');
+        if (VALID_VIEW_MODES.indexOf(mode) < 0) {
+            this.invalid = 'Invalid view mode. How did this happen!';
+            return false;
+        }
+        //It was in the list of valid modes
+        this.data.view = mode;
+
+        //Get the search string
+        this.data.search = $.trim(this.ui.search_input.val());
+
+        //Get the author string
+        this.data.author = $.trim(this.ui.author_input.val());
+
+        //Get whether or not to show retweets
+        this.data.rt = this.ui.rt_checkbox.is(':checked');
+
+        //Get the minimum RT count (validate first)
+        var num = Number(this.ui.min_rt_input.val());
+        if (isNaN(num) || num < 0) {
+            this.invalid = 'Not a valid minimum retweet count';
+            return false;
+        }
+        //Guess it was ok
+        this.data.min_rt = num;
+
+        //Get the selected sentiment filter, with validation
+        var sentiment = this.ui.sentiment_select.val();
+        if (VALID_SENTIMENTS.indexOf(sentiment) < 0) {
+            this.invalid = 'Invalid sentiment. How did this happen!';
+            return false;
+        }
+        this.data.sentiment = sentiment;
+
+        return true;
+    }
+
+    /**
+     * Place stored data into the query controls.
+     *
+     * @private
+     */
+    Query.prototype._fillForm = function () {
+        //Activate the proper mode button
+        this.ui.view_buttons.children().removeClass('active');
+        this.ui.view_buttons
+            .find('[data-mode=' + this.data.view + ']')
+            .addClass('active');
+
+        //Set the search string
+        this.ui.search_input.val(this.data.search);
+
+        //Set the author string
+        this.ui.author_input.val(this.data.author);
+
+        //Set the retweets checkbox
+        this.ui.rt_checkbox.prop('checked', this.data.rt);
+
+        //Set the min retweets filter
+        this.ui.min_rt_input.val(this.data.min_rt);
+
+        this.ui.sentiment_select.val(this.data.sentiment);
+    }
+
+    return Query;
+});
---
+++ tests/util/query_spec.js
@@ -0,0 +1,211 @@
+define(['jquery', 'util/query'],
+    function ($, Query) {
+
+        describe("Query", function () {
+
+            var ui,
+                viewButtonGroup,
+                viewButtons,
+                activeViewButton,
+                searchInput,
+                authorInput,
+                rtCheckbox,
+                minRTInput,
+                sentimentSelector,
+                updateButton;
+
+            var refreshUI = function() {
+                ui = $('form.query');
+
+                viewButtonGroup = ui.find('.view-buttons');
+                viewButtons = viewButtonGroup.find('button');
+                activeViewButton = viewButtons.filter('.active');
+
+                searchInput = ui.find('.query-search');
+                authorInput = ui.find('.query-author');
+                rtCheckbox = ui.find('.query-rt');
+                minRTInput = ui.find('.query-minrt');
+                sentimentSelector = ui.find('.query-sentiment');
+                updateButton = ui.find('.query-update');
+            }
+
+            beforeEach(function () {
+                loadFixtures('query.html');
+
+                refreshUI();
+            });
+
+            it('has default data', function() {
+                var query = new Query(ui);
+
+                expect(query.data.view).toEqual('area');
+                expect(query.data.search).toEqual('');
+                expect(query.data.author).toEqual('');
+                expect(query.data.rt).toEqual(false);
+                expect(query.data.sentiment).toEqual('');
+                expect(query.data.min_rt).toEqual(0);
+            });
+
+            it('extends provided data with defaults', function() {
+                var data = {
+                    search: 'a search string',
+                    sentiment: 'negative'
+                };
+
+                var query = new Query(ui, data);
+
+                expect(query.data.search).toEqual(data.search);
+                expect(query.data.sentiment).toEqual(data.sentiment);
+
+                expect(query.data.author).toEqual('');//default
+                expect(query.data.min_rt).toEqual(0);//default
+            });
+
+            it('applies data settings to query UI', function() {
+                //All non-defaults
+                var data = {
+                    view: 'stacked',
+                    search: 'a search string',
+                    author: '@ICanTweet',
+                    rt: true,
+                    min_rt: 2,
+                    sentiment: 'neutral'
+                };
+
+                var query = new Query(ui, data);
+
+                refreshUI();
+
+                expect(activeViewButton).toHaveData('mode', data.view);
+                expect(searchInput).toHaveValue(data.search.toString());
+                expect(authorInput).toHaveValue(data.author.toString());
+                expect(rtCheckbox).toBeChecked();
+                expect(minRTInput).toHaveValue(data.min_rt.toString());
+                expect(sentimentSelector).toHaveValue(data.sentiment.toString());
+            });
+
+            it('retrieves data settings from query UI', function() {
+                var query = new Query(ui);
+
+                var data = {
+                    view: 'stacked',
+                    search: 'a search string',
+                    author: '@ICanTweet',
+                    rt: true,
+                    min_rt: 2,
+                    sentiment: 'neutral'
+                };
+
+                //Disable all buttons
+                viewButtons.removeClass('active');
+                //Activate the right button
+                viewButtons.filter('[data-mode=' + data.view + ']').addClass('active');
+
+                searchInput.val(data.search);
+                authorInput.val(data.author);
+                rtCheckbox.prop('checked', true);
+                minRTInput.val(data.min_rt);
+                sentimentSelector.val(data.sentiment);
+
+                var result = query.collectData();
+
+                expect(result).toBeTruthy();
+                expect(query.data).toEqual(data);
+            });
+
+            it('recognizes invalid sentiment values', function() {
+                var query = new Query(ui);
+
+                //Have to add an option for this value in order to be able to set it
+                sentimentSelector.append('<option value="some random crap">HAHAHA</option>');
+                sentimentSelector.val('some random crap');
+
+                var result = query.collectData();
+
+                expect(result).toBeFalsy();
+                expect(query.invalid).toBeDefined();
+            });
+
+            it('recognizes invalid view modes', function() {
+                var query = new Query(ui);
+
+                activeViewButton.attr('data-mode', 'some random mode');
+
+                var result = query.collectData();
+
+                expect(result).toBeFalsy();
+                expect(query.invalid).toBeDefined();
+            });
+
+            it('recognizes invalid min rt values', function() {
+                var query = new Query(ui);
+
+                //Have to set the type to text to be able to set this value
+                minRTInput.attr('type', 'text');
+                minRTInput.val('not a number');
+                expect(minRTInput.val()).toEqual('not a number'); // just checking
+
+                var result = query.collectData();
+
+                expect(result).toBeFalsy();
+                expect(query.invalid).toBeDefined();
+            });
+
+            it('collects data on update click', function() {
+                var query = new Query(ui);
+
+                spyOn(query, 'collectData').andReturn(true);
+
+                expect(query.collectData).not.toHaveBeenCalled();
+
+                updateButton.click();
+
+                expect(query.collectData).toHaveBeenCalled();
+
+            });
+
+            it('fires an update event on update click', function() {
+                var query = new Query(ui);
+
+                spyOnEvent(query, 'update');
+
+                expect('update').not.toHaveBeenTriggeredOn(query);
+
+                updateButton.click();
+
+                expect('update').toHaveBeenTriggeredOn(query);
+            });
+
+            it('does not fire event on update click with invalid input', function() {
+                var query = new Query(ui);
+
+                activeViewButton.attr('data-mode', 'some random mode');
+
+                expect('update').not.toHaveBeenTriggeredOn(query);
+            });
+
+            it('fires a view-change event on view button click', function() {
+                var query = new Query(ui);
+
+                spyOnEvent(query, 'view-change');
+
+                expect('view-change').not.toHaveBeenTriggeredOn(query);
+
+                viewButtons.last().click();
+
+                expect('view-change').toHaveBeenTriggeredOn(query);
+            });
+
+            it('prevents form submit by enter and fires update', function() {
+                var query = new Query(ui);
+
+                spyOnEvent(query, 'update');
+                spyOnEvent(ui, 'submit');
+
+                ui.submit();
+
+                expect('submit').toHaveBeenPreventedOn(ui);
+                expect('update').toHaveBeenTriggeredOn(query);
+            });
+        });
+    });
