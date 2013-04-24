dd4bdfcd7c2cc1dacedaf62765e1200d0d340338
Michael Brooks <mjbrooks@uw.edu>
2013-04-24 00:07:44 -0700

    formatting updates

--- tests/util/events_spec.js
+++ tests/util/events_spec.js
@@ -1,32 +1,34 @@
-define(['util/events'],
-    function(events) {
+define(['jquery', 'util/events'],
+    function ($, events) {
 
-        describe("jQuery Events Mixin", function() {
+        describe("jQuery Events Mixin", function () {
 
             var MyClass;
 
-            beforeEach(function() {
-                MyClass = function() {};
-            }
-
-            it('adds methods to prototype', function() {
-                events(MyClass);
-                expect(MyClass.prototype.on).toBeDefined());
-                expect(MyClass.prototype.trigger).toBeDefined());
+            beforeEach(function () {
+                MyClass = function () {
+                };
             });
 
-            it('allows fires events to listeners', function() {
+            it('adds methods to prototype', function () {
+                events(MyClass);
+                expect(MyClass.prototype.on).toBeDefined();
+                expect(MyClass.prototype.trigger).toBeDefined();
+                expect(MyClass.prototype.off).toBeDefined();
+            });
+
+            it('allows fires events to listeners', function () {
                 events(MyClass);
 
                 var foo = new MyClass();
 
                 var firstCallback = 0;
-                foo.on('asdf', function() {
+                foo.on('asdf', function () {
                     firstCallback++;
                 });
 
                 var secondCallback = 0;
-                foo.on('asdf', function() {
+                foo.on('asdf', function () {
                     secondCallback++;
                 });
 
@@ -35,5 +37,58 @@ define(['util/events'],
                 expect(firstCallback).toEqual(1);
                 expect(secondCallback).toEqual(1);
             });
+
+            it('transmits data with events', function () {
+                events(MyClass);
+
+                var foo = new MyClass();
+
+                var event = null;
+                var args = null;
+                foo.on('asdf', function (e) {
+                    event = e;
+                    args = Array.prototype.slice.call(arguments, 1);
+                });
+
+                foo.trigger('asdf');
+
+                expect(event).toEqual(jasmine.any($.Event));
+                expect(args).toEqual([]);
+
+                foo.trigger('asdf', 'a', 'b', 'c');
+                expect(event).toEqual(jasmine.any($.Event));
+                expect(args).toEqual(['a', 'b', 'c']);
+            });
+
+            it('allows unsubscribing from events', function () {
+                events(MyClass);
+
+                var foo = new MyClass();
+
+                var callCount = 0;
+                var callback = function () {
+                    callCount++;
+                }
+
+
+                foo.on('asdf', callback);
+                foo.trigger('asdf');
+                expect(callCount).toEqual(1);
+
+                //Unsubscribe specifically
+                foo.off('asdf', callback);
+                foo.trigger('asdf');
+                expect(callCount).toEqual(1);
+
+                //Resubscribe
+                foo.on('asdf', callback);
+                foo.trigger('asdf');
+                expect(callCount).toEqual(2);
+
+                //Unsubscribe globally
+                foo.off('asdf');
+                foo.trigger('asdf');
+                expect(callCount).toEqual(2);
+            })
         });
     });
