d98ebf6be149547bb78a3478a837a9d7428f2cb7
Michael Brooks <mjbrooks@uw.edu>
2013-04-24 00:50:04 -0700

    restoring some functionality to app

--- js/app.js
+++ js/app.js
@@ -24,10 +24,9 @@ define(['jquery', 'underscore',
          */
         var App = function () {
 
-            this.initQueryModels();
             this.initUI();
 
-            this.initQueryPanel();
+            this.initQueries();
 
             this.initContextTimeline();
             this.initFocusTimeline();
@@ -41,14 +40,45 @@ define(['jquery', 'underscore',
         /**
          * Set up the query object, based on the url.
          */
-        App.prototype.initQueryModels = function () {
-            this.query = {};
+        App.prototype.initQueries = function () {
+            this.queries = [];
+            this.interval = {};
 
             var params = document.location.search;
 
-            this.query.from = getParameterByName(params, 'from', SB_START) * 1000;
-            this.query.to = getParameterByName(params, 'to', SB_END) * 1000;
-            this.query.search = getParameterByName(params, 'search', null);
+            this.interval.from = getParameterByName(params, 'from', SB_START) * 1000;
+            this.interval.to = getParameterByName(params, 'to', SB_END) * 1000;
+
+            this.ui.queryPanel = $('#queries');
+
+            var self = this;
+            this.ui.queryPanel.find('.query')
+                .each(function (index) {
+                    var data = {};
+
+                    var view = getParameterByName(params, parameterName('view', index), null);
+                    var search = getParameterByName(params, parameterName('search', index), null);
+                    var author = getParameterByName(params, parameterName('author', index), null);
+                    var rt = getParameterByName(params, parameterName('rt', index), null);
+                    var min_rt = getParameterByName(params, parameterName('min_rt', index), null);
+                    var sentiment = getParameterByName(params, parameterName('sentiment', index), null);
+
+                    if (view !== null) data.view = view;
+                    if (search !== null) data.search = search;
+                    if (author !== null) data.author = author;
+                    if (rt !== null) data.rt = rt;
+                    if (min_rt !== null) data.min_rt = min_rt;
+                    if (sentiment !== null) data.sentiment = sentiment;
+
+                    var ui = $(this);
+                    var query = new Query(ui, data);
+
+                    query.on('update', $.proxy(self.queryUpdated, self));
+                    query.on('view-change', $.proxy(self.queryViewChanged, self));
+
+                    self.queries.push(query);
+                });
+
         }
 
         /**
@@ -84,39 +114,15 @@ define(['jquery', 'underscore',
                 .retweetHeight(70)
                 .noiseHeight(70)
                 .noiseThreshold(min_important_rt)
-                .searchQuery(this.query.search)
+//                .searchQuery(this.query.search)
                 .utcOffsetMillis(utcOffsetMillis)
                 .idealBinCount(200)
-                .timeExtent([this.query.from, this.query.to])
-                .onZoomChanged(function (extent) {
-
-                    //When the timeline zoom/pan changes, we need to update the query object
-                    self.query.from = extent[0];
-                    self.query.to = extent[1];
-
-                    //and update the url
-                    self.updateUrl();
-                });
+                .timeExtent([this.interval.from, this.interval.to])
+                .onZoomChanged($.proxy(self.zoomChanged, self));
 
             //Set the container and render
             this.focusTimeline.container(this.ui.focusTimeline.selector)
                 .render();
-        }
-
-        /**
-         * Set up the search box.
-         */
-        App.prototype.initQueryPanel = function () {
-            this.ui.queryPanel = $('#queries');
-
-            var queries = this.ui.queryPanel.find('.query');
-
-            queries.each(function(index) {
-                var ui = $(this);
-                var query = new Query(ui);
-            });
-
-            var self = this;
         }
 
         /**
@@ -128,7 +134,7 @@ define(['jquery', 'underscore',
 
             this.tweetList = new TweetList(this.ui.tweetList);
             //Load the tweets for the current query
-            this.tweetList.update(this.query);
+//            this.tweetList.update(this.query);
         }
 
         App.prototype.initDetailsPanel = function () {
@@ -149,18 +155,37 @@ define(['jquery', 'underscore',
          * Update the url based on the current query.
          */
         App.prototype.updateUrl = function () {
-            //var search = this.ui.searchInput.val();
-            //var from = this.query.from / 1000;
-            //var to = this.query.to / 1000;
+            var params = {
+                from: Math.round(this.interval.from / 1000),
+                to: Math.round(this.interval.to / 1000)
+            };
 
-            //var state = {
-            //    search: search,
-            //    from: from,
-            //    to: to
-            //}
+            this.queries.forEach(function (query, index) {
+                for (var key in query.data) {
+                    params[parameterName(key, index)] = query.data[key];
+                }
+            });
 
             //Fancy HTML5 history management
-            //history.pushState(state, '', '?' + $.param(state));
+            history.pushState(params, '', '?' + $.param(params));
+        }
+
+
+        App.prototype.queryUpdated = function (query) {
+            this.updateUrl();
+        }
+
+        App.prototype.queryViewChanged = function (query) {
+            this.updateUrl();
+        }
+
+        App.prototype.zoomChanged = function (extent) {
+            //When the timeline zoom/pan changes, we need to update the query object
+            this.interval.from = extent[0];
+            this.interval.to = extent[1];
+
+            //and update the url
+            this.updateUrl();
         }
 
         /**
@@ -175,6 +200,26 @@ define(['jquery', 'underscore',
                 return defaultValue;
             else
                 return decodeURIComponent(results[1].replace(/\+/g, " "));
+        }
+
+        var PARAMETER_NAME_MAP = {
+            view: 'v',
+            search: 'q',
+            author: 'a',
+            rt: 'r',
+            min_rt: 'm',
+            sentiment: 'f'
+        };
+
+        /**
+         * Given a nice parameter name (i.e. 'view') and
+         * the query index (1, 2), generate the short url name.
+         *
+         * @param name
+         * @param queryIndex
+         */
+        function parameterName(name, queryIndex) {
+            return PARAMETER_NAME_MAP[name] + queryIndex.toString();
         }
 
         //Start the app
