85b3f6847d6c44db0def0f0e3d71816f3307082d
Michael Brooks <mjbrooks@uw.edu>
2013-05-01 20:52:03 -0700

    api class

---
+++ js/util/api.js
@@ -0,0 +1,150 @@
+define(['jquery', 'underscore', 'util/events'], function($, _, events) {
+
+    /**
+     * A map from API request names to urls.
+     */
+    var URLS = {
+        'counts': 'data/counts.php'
+    };
+
+    /**
+     * API is an object that knows how to asynchronously
+     * request data from the server and send data to the server.
+     *
+     * It is in charge of all URLs, but knows nothing about the data it is sending
+     * or receiving.
+     *
+     * Since requests may be fulfilled out of order, it filters the
+     * results at the query name level to ensure that data
+     * returned is never older than some other data that was already
+     * returned.
+     *
+     * Data events are triggered with an object containing request params and the data (result).
+     */
+    var API = function() {
+
+        this.urls = {};
+        this.rid_counters = {};
+
+        for (var name in URLS) {
+            //Initialize the local URLS dictionary
+            this.urls[name] = URLS[name];
+
+            //Initialize the request id counters
+            this.rid_counters[name] = {
+                sent: 0,
+                received: 0
+            };
+        }
+    };
+
+    /**
+     * Method to register a new URL after the fact, mostly useful for testing.
+     *
+     * @param name the request type name
+     * @param url the url to request from
+     * @param [method] an optional shorthand method to add to the API object
+     */
+    API.prototype.register = function(name, url, method) {
+        this.rid_counters[name] = {
+            sent: 0,
+            received: 0
+        };
+
+        this.urls[name] = url;
+
+        if (method) {
+            this[name] = method;
+        }
+    };
+
+    API.prototype.get_last_rid_sent = function(name){
+        return this.rid_counters[name].sent;
+    };
+
+    API.prototype.get_last_rid_received = function(name){
+        return this.rid_counters[name].received;
+    };
+
+    /**
+     * The workhorse method for all API requests.
+     * Request parameters must already be prepared for sending to the server.
+     *
+     * The optional parameters argument can include:
+     * - params: an object containing the request parameters.
+     * - post_process: a function to post-process the results before distribution
+     *
+     * @param method 'get', 'post'
+     * @param name the API url name
+     * @param [parameters] {{}}
+     * @private
+     */
+    API.prototype.request = function(method, name, parameters) {
+        parameters = parameters || {};
+
+        var params = parameters.params || {};
+        var post_process = parameters.post_process;
+
+        //Make a new request id
+        this.rid_counters[name].sent++;
+
+        //Add the request id to the options
+        var rid = this.rid_counters[name].sent;
+
+        //Issue an AJAX request
+        var r = $.ajax({
+            url: this.urls[name],
+            data: params,
+            type: method
+        });
+
+        var self = this;
+        r.done(function(result) {
+
+            //Make sure the request is more recent than the last one received for this name
+            var lastReceived = self.rid_counters[name].received;
+            if (lastReceived >= rid) {
+                return;
+            }
+
+            //This is the last one received for this name
+            self.rid_counters[name].received = rid;
+
+            //Push the params through a post processing step, if provided
+            if (post_process) {
+                result = post_process(result);
+            }
+
+            //Trigger a new params event, providing the params and result
+            self.trigger(name, {
+                params: params,
+                data: result
+            });
+        });
+    };
+
+    /**
+     * Request tweet counts.
+     *
+     * Parameters should include the necessary arguments for the server
+     * to process the request.
+     *
+     * In order to receive the results, you should subscribe to the 'counts' event.
+     *
+     * @param parameters
+     */
+    API.prototype.counts = function(parameters) {
+        this.request('get', 'counts', {
+            params: parameters,
+            post_process: function (results) {
+                //Post-processing is just extracting the payload
+                return results.payload;
+            }
+        });
+    };
+
+    //Mix in events
+    events(API);
+
+    return API;
+});
---
+++ tests/util/api_spec.js
@@ -0,0 +1,114 @@
+define(['util/api', 'jquery'], function(API, $) {
+
+    describe('API', function() {
+
+        var myResponse;
+
+        beforeEach(function() {
+            myResponse = $.Deferred();
+        });
+
+
+        it('calls requests from shorthand method', function() {
+            var api = new API();
+
+            spyOn(api, 'request');
+
+            var params = {foo: 'bar'};
+
+            api.counts(params);
+
+            expect(api.request).toHaveBeenCalledWith('get', 'counts', {
+                params: params,
+                post_process: jasmine.any(Function)
+            });
+        });
+
+        it('increments request ids when requests go out', function() {
+            var api = new API();
+
+            //Just prevent ajax calls
+            spyOn($, 'ajax').andReturn(myResponse);
+
+            expect(api.get_last_rid_sent('counts')).toEqual(0);
+            expect(api.get_last_rid_received('counts')).toEqual(0);
+
+            api.request('get', 'counts');
+
+            expect(api.get_last_rid_sent('counts')).toEqual(1);
+            expect(api.get_last_rid_received('counts')).toEqual(0);
+        });
+
+        it('increments request ids when requests come in', function() {
+            var api = new API();
+
+            //Just prevent ajax calls
+            spyOn($, 'ajax').andReturn(myResponse);
+
+            expect(api.get_last_rid_sent('counts')).toEqual(0);
+            expect(api.get_last_rid_received('counts')).toEqual(0);
+
+            api.request('get', 'counts');
+
+            expect(api.get_last_rid_sent('counts')).toEqual(1);
+            expect(api.get_last_rid_received('counts')).toEqual(0);
+
+            myResponse.resolve({
+                payload: 'asdf'
+            });
+
+            expect(api.get_last_rid_sent('counts')).toEqual(1);
+            expect(api.get_last_rid_received('counts')).toEqual(1);
+        });
+
+        it('triggers the right event when the request is fulfilled', function() {
+            var api = new API();
+
+            //Just prevent ajax calls
+            spyOn($, 'ajax').andReturn(myResponse);
+
+            spyOnEvent(api, 'counts');
+
+            api.request('get', 'counts');
+
+            expect('counts').not.toHaveBeenTriggeredOn(api);
+
+            myResponse.resolve({
+                payload: 'asdf'
+            });
+
+            expect('counts').toHaveBeenTriggeredOn(api);
+        });
+
+        it('can make new kinds of requests', function() {
+            var api = new API();
+
+            //Just prevent ajax calls
+            spyOn($, 'ajax').andReturn(myResponse);
+
+            spyOnEvent(api, 'newrequest');
+
+            var requester = function() {};
+
+            api.register('newrequest', 'http://example.com', requester);
+
+            expect(api.newrequest).toBe(requester);
+
+            api.request('get', 'newrequest');
+
+            myResponse.resolve({
+                payload: 'asdf'
+            });
+
+            expect('newrequest').toHaveBeenTriggeredOn(api);
+
+            expect($.ajax).toHaveBeenCalledWith({
+                url: 'http://example.com',
+                data: {},
+                type: 'get'
+            });
+        });
+
+    });
+
+});
