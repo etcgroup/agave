c9bc302c75c83077f81b05517aae7246ae0fdc3e
Megan Torkildson <mtorkild@uw.edu>
2013-05-12 21:22:56 -0700

    fixing layout

--- templates/layout.php
+++ templates/layout.php
@@ -34,6 +34,7 @@
                                         <li><a data-target="#users-list-1" data-toggle="tab">Users</a></li>
                                         <li><a data-target="#keywords-list-1" data-toggle="tab">Keywords</a></li>
                                     </ul>
+
                                     <div class="tab-content row scroll-y">
                                         <div class="tab-pane active tweet-list" id="tweet-list-1"></div>
                                         <div class="tab-pane users-list" id="users-list-1"></div>
@@ -41,81 +42,84 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="details-right col">
-                                <div class="padding-left-half">
-                                    <?php echo query_box('Series 2') ?>
-                                    <div class="tab-group">
-                                        <ul class="nav nav-tabs row">
-                                            <li class="active"><a data-target="#tweet-list-2" data-toggle="tab">Tweets</a></li>
-                                            <li><a data-target="#users-list-2" data-toggle="tab">Users</a></li>
-                                            <li><a data-target="#keywords-list-2" data-toggle="tab">Keywords</a></li>
-                                        </ul>
-                                        <div class="tab-content row scroll-y">
-                                            <div class="tab-pane active tweet-list" id="tweet-list-2"></div>
-                                            <div class="tab-pane users-list" id="users-list-2"></div>
-                                            <div class="tab-pane keywords-list" id="keywords-list-2">...</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
-                        <div class="collaborator-wrapper col">
-                            <div class="collaborator padding-left show-left">
-                                <div class="sliding-panel col">
-                                    <div class="user-box form-inline col">
-                                        <input type="text" placeholder="Choose a user name"/>
-                                        <button type="button" class="user-submit btn btn-primary">Sign in</button>
-                                    </div>
-                                    <div class="discussions col">
-                                        <div class="header row">
-                                            <div class="title">Discussions</div>
-                                            <button type="button" class="btn new-button">New Discussion</button>
-                                            <p>Join an existing discussion:</p>
-                                        </div>
-                                        <div class="discussion-list row scroll-y"></div>
-                                    </div>
-                                    <div class="discussion-view col">
-                                        <div class="comment-box row">
-                                            <button type="button" class="btn back-button">
-                                                <i class="icon-arrow-left"></i> Back to discussion list
-                                            </button>
-                                            <br/>
-                                            <textarea></textarea>
-                                            <span class="user-display"></span>
-                                            <button type="button"
-                                                    class="send-button btn btn-primary">
-                                                <i class="icon-white icon-comment"></i> Post message
-                                            </button>
-                                        </div>
-                                        <div class="comments row scroll-y"></div>
+                        <div class="details-right col">
+                            <div class="padding-left-half">
+                                <?php echo query_box('Series 2') ?>
+                                <div class="tab-group">
+                                    <ul class="nav nav-tabs row">
+                                        <li class="active"><a data-target="#tweet-list-2" data-toggle="tab">Tweets</a></li>
+                                        <li><a data-target="#users-list-2" data-toggle="tab">Users</a></li>
+                                        <li><a data-target="#keywords-list-2" data-toggle="tab">Keywords</a></li>
+                                    </ul>
+
+                                    <div class="tab-content row scroll-y">
+                                        <div class="tab-pane active tweet-list" id="tweet-list-2"></div>
+                                        <div class="tab-pane users-list" id="users-list-2"></div>
+                                        <div class="tab-pane keywords-list" id="keywords-list-2">...</div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div class="collaborator-wrapper col">
+                    <div class="collaborator padding-left show-left">
+                        <div class="sliding-panel col">
+                            <div class="user-box form-inline col">
+                                <input type="text" placeholder="Choose a user name"/>
+                                <button type="button" class="user-submit btn btn-primary">Sign in</button>
+                            </div>
+                            <div class="discussions col">
+                                <div class="header row">
+                                    <div class="title">Discussions</div>
+                                    <button type="button" class="btn new-button">New Discussion</button>
+                                    <p>Join an existing discussion:</p>
+                                </div>
+                                <div class="discussion-list row scroll-y"></div>
+                            </div>
+                            <div class="discussion-view col">
+                                <div class="comment-box row">
+                                    <button type="button" class="btn back-button">
+                                        <i class="icon-arrow-left"></i> Back to discussion list
+                                    </button>
+                                    <br/>
+                                    <textarea></textarea>
+                                    <span class="user-display"></span>
+                                    <button type="button"
+                                            class="send-button btn btn-primary">
+                                        <i class="icon-white icon-comment"></i> Post message
+                                    </button>
+                                </div>
+                                <div class="comments row scroll-y"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-                <script>
-                    window.require = {
-                        baseUrl: 'js',
-                        paths: {
-                            'underscore': 'lib/underscore-amd',
-                            'backbone': 'lib/backbone-amd',
-                            'jquery': 'lib/jquery',
-                            'moment': 'lib/moment'
-                        },
-                        shim: {
-                            'lib/d3': {
-                                exports: 'd3'
-                            },
-                            'lib/bootstrap': ['jquery'],
-                            'lib/Uri': {
-                                exports: 'Uri'
-                            }
-                        }
-                    };
-                </script>
+        <script>
+            window.require = {
+                baseUrl: 'js',
+                paths: {
+                    'underscore': 'lib/underscore-amd',
+                    'backbone': 'lib/backbone-amd',
+                    'jquery' : 'lib/jquery',
+                    'moment': 'lib/moment'
+                },
+                shim: {
+                    'lib/d3': {
+                        exports: 'd3'
+                    },
+                    'lib/bootstrap': ['jquery'],
+                    'lib/Uri': {
+                        exports: 'Uri'
+                    }
+                }
+            };
+        </script>
 
-                <script src="js/lib/require.js" data-main="main"></script>
-                </body>
-                </html>
+        <script src="js/lib/require.js" data-main="main"></script>
+    </body>
+</html>
