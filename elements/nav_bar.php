4e6d5ea5934b9c0caf1cf598669b52f2b00f5afc
Michael Brooks <mjbrooks@uw.edu>
2013-05-16 00:40:41 -0700

    update bootstrap styles

--- elements/nav_bar.php
+++ elements/nav_bar.php
@@ -10,10 +10,10 @@ function nav_bar()
                 <a class="brand" href="#">TwitterVis</a>
                 <ul class="nav">
                     <li>
-                        <span class="title">Super Bowl 47</span>
+                        <span class="title navbar-text">Super Bowl 47</span>
                     </li>
                     <li>
-                        <span class="muted">2/3/2013 6:30pm - 2/3/2013 10:00pm EST; 7.5 million tweets, 3.9 million authors</span>
+                        <span class="navbar-text">2/3/2013 6:30pm - 2/3/2013 10:00pm EST; 7.5 million tweets, 3.9 million authors</span>
                     </li>
                 </ul>
             </div>
