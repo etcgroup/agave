77f51a831f4a8af94e024d3eebe74de552cbbdd7
Michael Brooks <mjbrooks@uw.edu>
2013-05-20 16:12:51 -0700

    switch to created_at_5s

--- data/util/binder.php
+++ data/util/binder.php
@@ -31,6 +31,21 @@ class Binder
         return "(:$param)";
     }
 
+    public function grouped_created_at_params($start_datetime, $stop_datetime, $group_seconds=5) {
+        if ($group_seconds !== 5) {
+            throw new Exception("Group seconds must be 5!");
+        }
+
+
+        $start_datetime = $start_datetime->getTimestamp() / $group_seconds;
+        $stop_datetime = $stop_datetime->getTimestamp() / $group_seconds;
+
+        $start_datetime = $this->param('from', $start_datetime, PDO::PARAM_INT);
+        $stop_datetime = $this->param('to', $stop_datetime, PDO::PARAM_INT);
+
+        return array($start_datetime, $stop_datetime, $group_seconds);
+    }
+
     /**
      * Get the keys for the parameters.
      *
