442997ad9cde800d3fc423642819c61409470fee
Michael Brooks <mjbrooks@uw.edu>
2013-08-14 00:46:49 -0700

    discussion search ui

--- elements/discussion.inc.php
+++ elements/discussion.inc.php
@@ -7,10 +7,18 @@ function discussion($row)
 {
     $subject = preg_replace('/[AT]\d+\[/', '[', $row['subject']);
 
+    $message_count = $row['message_count'];
+    $message_count_tooltip = 'Message Count';
+    if (isset($row['match_count'])) {
+        $message_count = $row['match_count'] . ' / ' . $message_count;
+        $message_count_tooltip = 'Matches / Message Count';
+    }
+
     ob_start();
     ?>
     <li class="item discussion clearfix" data-id="<?php echo $row['id']?>">
-        <div class="message-count badge tooltip-me" Title="Message Count"><?php echo $row['message_count'] ?></div>
+        <div class="message-count badge tooltip-me"
+             title="<?php echo $message_count_tooltip ?>"><?php echo $message_count ?></div>
         <div class="subject"><?php echo $subject ?></div>
         <div class="last-comment-at muted">Updated <?php echo ago($row['last_comment_at']) ?> ago</div>
     </li>
